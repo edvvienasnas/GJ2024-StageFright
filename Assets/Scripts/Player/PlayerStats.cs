e907fa25e2f812b05e63d0a4fc9df68cf3218757
edvvienasnas <97709285+edvvienasnas@users.noreply.github.com>
2024-01-27 11:36:10 +0200

    Add player hp indicators

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private int hp = 3;
+    [SerializeField] private List<Image> hpIndicator;
 
     private void Update()
     {
@@ -22,6 +25,10 @@ public class PlayerStats : MonoBehaviour
         if(other.tag == "Enemy")
         {
             hp--;
+            if(hp == 2) hpIndicator[2].enabled = false;
+            else if(hp == 1) hpIndicator[1].enabled = false;
+            else if(hp <= 0) hpIndicator[0].enabled = false;
+
             Debug.Log("You got hit");
         }
     }
